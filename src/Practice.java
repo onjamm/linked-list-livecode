37b682b1ce5e67c26fea22cd7126f33cd394e3a9
onjamm <azmionjang@gmail.com>
2026-04-28 14:58:25 -0700

    Finished the livecode, forgot to push whoops!

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,23 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        
+        Node myNode = new Node('w', null);
+        Node otherNode = new Node('x', null);
+        Node coolNode = new Node('e', null);
+        
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+
+        Node current = myNode;
+
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
+
+        //Going down the line of nodes
+        System.out.println(myNode.value);
+        System.out.println(myNode.next.value);
+        System.out.println(myNode.next.next.value);
     }
 }
